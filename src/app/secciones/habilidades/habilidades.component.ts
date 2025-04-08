94322dda563c5a6da9d5432d5c69c92eebb62de0
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-04-08 17:02:06 +0200

    Proyecto porfolio creado

---
+++ src/app/secciones/habilidades/habilidades.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habilidades',
+  imports: [],
+  templateUrl: './habilidades.component.html',
+  styleUrl: './habilidades.component.css'
+})
+export class HabilidadesComponent {
+
+}
---
+++ src/app/secciones/testimonios/testimonios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonios',
+  imports: [],
+  templateUrl: './testimonios.component.html',
+  styleUrl: './testimonios.component.css'
+})
+export class TestimoniosComponent {
+
+}
