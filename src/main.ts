c2e0b1cfd1ae8fa3986e51c2561541d27c4f16c7
Rodrigo Pozo Sanchez <rodrigopozosanchez@gmail.com>
2025-05-02 16:25:08 +0000

    perfecto ahora añade dos botones al lado de info con el icono de github y cada uno con un span front y el otro back con sus respectivos links.
    https://github.com/rodrigopozodev/ZapasPro_Back
    
    y para el front https://github.com/rodrigopozodev/ZapasPro_Front

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
-import { LucideAngularModule, Github, ExternalLink, Mail, Download, Send, Sun, Moon, Info, Linkedin } from 'lucide-angular'; // Added Linkedin icon
+import { LucideAngularModule, Github, ExternalLink, Mail, Download, Send, Sun, Moon, Info, Linkedin } from 'lucide-angular'; // Added Github icon
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes),
     provideHttpClient(),
-    importProvidersFrom(LucideAngularModule.pick({ Github, ExternalLink, Mail, Download, Send, Sun, Moon, Info, Linkedin })) // Added Linkedin icon
+    importProvidersFrom(LucideAngularModule.pick({ Github, ExternalLink, Mail, Download, Send, Sun, Moon, Info, Linkedin })) // Added Github icon
   ]
 }).catch(err => console.error(err));
+
 
