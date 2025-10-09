f630198516665cb89c3c280721a77c525ce4ed4a
Rodrigo Pozo <rodri@example.com>
2025-10-09 11:25:39 +0200

    eliminando librerias que no uso en la pagina de tecnologias

--- src/app/components/tecnologias/tecnologias.component.ts
+++ src/app/components/tecnologias/tecnologias.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LucideAngularModule } from 'lucide-angular';
 import { TranslatePipe } from '@/pipes/translate.pipe';
 
 @Component({
   selector: 'app-tecnologias',
   standalone: true,
   templateUrl: './tecnologias.component.html',
-  imports: [CommonModule, LucideAngularModule, TranslatePipe],
+  imports: [CommonModule],
 })
 export class TecnologiasComponent {}
