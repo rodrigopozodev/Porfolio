4e58986362de77aa992d753e491ec9d9d874bd54
rodri <rodri@example.com>
2025-11-17 12:10:25 +0100

    ajustando errores

--- version-2/app/page.tsx
+++ version-2/app/page.tsx
@@ -1,4 +1,25 @@
+"use client"
 /**
- * Entrada principal que reexporta la página de Inicio.
+ * Página Inicio: define el grid principal y monta las secciones.
  */
-export { default } from "./pages/inicio/page"
+
+import React from "react"
+import HeaderInicio from "./pages/inicio/sections/Header/HeaderInicio"
+import "./pages/inicio/styles/grid.css"
+import "./pages/inicio/styles/inicio.css"
+import "./pages/inicio/styles/responsive.css"
+
+export default function PageInicio() {
+  return (
+    <main className="parent">
+      <div className="header">
+        <HeaderInicio />
+      </div>
+      <div className="nav-left box">Navegación izquierda</div>
+      <div className="recomendaciones box">Recomendaciones</div>
+      <div className="personal box">Personal</div>
+      <div className="destacado box">Proyecto destacado</div>
+      <div className="nav-right box">Navegación derecha</div>
+    </main>
+  )
+}
