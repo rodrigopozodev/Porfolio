238a05d57d0cfe6d225fbf11ea7c8b44769fa58e
rodri <rodri@example.com>
2025-11-18 00:05:44 +0100

    Creacion basica de la pagina "Proyectos"

---
+++ version-2/app/componentes/paginas/proyectos/Proyectos.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React from "react"
+
+const Proyectos = () => {
+  return (
+    <main className="parent" aria-label="Proyectos">
+      <div className="header box">Proyectos</div>
+      <div className="box" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+        Próximamente: listado y destacados
+      </div>
+      <div className="box" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+        Sección auxiliar
+      </div>
+    </main>
+  )
+}
+
+export default Proyectos
--- version-2/app/page.tsx
+++ version-2/app/page.tsx
@@ -3,16 +3,24 @@
 import HeaderInicio from "./componentes/paginas/inicio/Header/HeaderInicio"
 import BodyInicio from "./componentes/paginas/inicio/body/bodyinicio"
 import NavegacionInicio from "./componentes/paginas/inicio/navegacion/navegacioninicio"
+import Proyectos from "./componentes/paginas/proyectos/Proyectos"
 import "./componentes/paginas/inicio/styles/grid.css"
 import "./componentes/paginas/inicio/styles/inicio.css"
 export default function PageInicio() {
   return (
-    <main className="parent">
-      <div className="header">
-        <HeaderInicio />
-      </div>
-      <NavegacionInicio />
-      <BodyInicio />
-    </main>
+    <div className="snap-container">
+      <section id="inicio" className="page-section">
+        <main className="parent">
+          <div className="header">
+            <HeaderInicio />
+          </div>
+          <NavegacionInicio />
+          <BodyInicio />
+        </main>
+      </section>
+      <section id="proyectos" className="page-section">
+        <Proyectos />
+      </section>
+    </div>
   )
 }
